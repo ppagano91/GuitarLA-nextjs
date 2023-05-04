5de4d9d5e34cd12ccfffbbaeda82a3feae768c97
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-05-04 20:01:52 -0300

    Creando Footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,32 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "@/styles/Footer.module.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div className="contenedor">
+        <Link href="/" legacyBehavior>
+          <a>
+            <Image
+              src="/img/logo.svg"
+              width={300}
+              height={40}
+              alt={"Imagen logo"}
+            />
+          </a>
+        </Link>
+        <nav className={styles.navegacion}>
+          <Link href="/">Inicio</Link>
+          <Link href="/nosotros">Nosotros</Link>
+          <Link href="/blog">Blog</Link>
+          <Link href="/tienda">Tienda</Link>
+        </nav>
+        <p>Todos los derechos reservados {new Date().getFullYear()}</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
