2c923aab2374bba0dd6db710b812d09cc68a3230
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-05-04 09:35:24 -0300

    Probando Routing con componente Link

---
+++ src/pages/nosotros.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Nosotros = () => {
+  return (
+    <>
+    <div>Nosotros</div>
+    <Link href="/">Ir a Inicio</Link>
+    </>
+  )
+}
+
+export default Nosotros
