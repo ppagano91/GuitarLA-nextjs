d2d31100ec27e7ea7a0bf23bf9e261166fb250ec
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-05-04 15:18:17 -0300

    Agregando estilos a Header

--- src/components/header.js
+++ src/components/header.js
@@ -2,19 +2,20 @@ import React from "react";
 import Image from "next/image";
 // import logo from "../../public/img/logo.svg";
 import Link from "next/link";
+import styles from "@/styles/Header.module.css";
 
 const Header = () => {
   return (
-    <header>
-      <div className="contenedor">
+    <header className={styles.header}>
+      <div className={`contenedor ${styles.barra}`}>
         <Image src="/img/logo.svg" width={300} height={40} />
+        <nav className={styles.navegacion}>
+          <Link href="/">Inicio</Link>
+          <Link href="/nosotros">Nosotros</Link>
+          <Link href="/blog">Blog</Link>
+          <Link href="/tienda">Tienda</Link>
+        </nav>
       </div>
-      <nav>
-        <Link href="/">Inicio</Link>
-        <Link href="/nosotros">Nosotros</Link>
-        <Link href="/blog">Blog</Link>
-        <Link href="/tienda">Tienda</Link>
-      </nav>
     </header>
   );
 };
