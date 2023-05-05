4a3233eb585e1d123aa341894db0fd01918e3a0a
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-05-05 08:29:24 -0300

    Consultando la API con getStaticProps()

--- src/pages/tienda.js
+++ src/pages/tienda.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Layout from "@/components/layout";
+import ListadoGuitarras from "@/components/listado-guitarras";
 
-const Tienda = () => {
+const Tienda = ({ guitarras }) => {
+  console.log(guitarras);
   return (
     <>
       <Layout
@@ -10,10 +12,22 @@ const Tienda = () => {
           "Tienda virtual, venta de guitarras, instrumentos y mucho más"
         }
       >
-        <div>Tienda</div>
+        <main className="contenedor">
+          <h2 className="heading">Nuestra Colección</h2>
+          <ListadoGuitarras />
+        </main>
       </Layout>
     </>
   );
 };
 
 export default Tienda;
+
+export async function getStaticProps() {
+  const respuesta = await fetch(
+    `${process.env.API_URL}/guitarras?populate=imagen`
+  );
+  const { data: guitarras } = await respuesta.json();
+  // console.log(data);
+  return { props: { guitarras } };
+}
