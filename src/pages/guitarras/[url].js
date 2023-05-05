696c3c509293635358903cd8166130974b6db07c
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-05-05 09:02:13 -0300

    Routing Dinamico con getServerSideProps

--- src/pages/guitarras/[url].js
+++ src/pages/guitarras/[url].js
@@ -1,7 +1,18 @@
 import React from "react";
 
-const Producto = () => {
+const Producto = ({ guitarra }) => {
+  console.log(guitarra);
   return <div>Producto</div>;
 };
 
 export default Producto;
+
+export async function getServerSideProps({ query: { url } }) {
+  const respuesta = await fetch(
+    `${process.env.API_URL}/guitarras?filters[url]=${url}&populate=imagen`
+  );
+  const { data: guitarra } = await respuesta.json();
+  return {
+    props: { guitarra },
+  };
+}
