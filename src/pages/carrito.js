7954ddb7580796a4931a1cb23c84815bc4c90de4
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-05-08 10:03:13 -0300

    Agregando estilos

--- src/pages/carrito.js
+++ src/pages/carrito.js
@@ -9,11 +9,11 @@ const Carrito = () => {
     <Layout title="Carrito de Compras">
       <main className="contenedor">
         <h1 className="heading">Carrito</h1>
-        <div>
-          <div>
+        <div className={styles.contenido}>
+          <div className={styles.carrito}>
             <h2>Articulos</h2>
           </div>
-          <aside>
+          <aside className={styles.resumen}>
             <h3>Resumen del Pedido</h3>
             <p>Total a pagar</p>
           </aside>
