a4ee4d9ad59c6e91cb4f87484b23cd104c8a16bb
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-05-08 09:32:34 -0300

    Mostrando Guitarras y Blog en la página de Inicio

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,60 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+// import styles from "@/styles/Home.module.css";
+
 import Layout from "@/components/layout";
+import Guitarra from "@/components/guitarra";
+import Post from "@/components/post";
+import styles from "@/styles/Grid.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function Home({ guitarras, posts }) {
   return (
     <>
       <Layout
         title={"Inicio"}
         description={"Blog de música, venta de guitarras y más"}
       >
-        <h1>Hello World!</h1>
+        <main className={`contenedor`}>
+          <h1 className="heading">Nuestra Colección</h1>
+          <div className={styles.grid}>
+            {guitarras?.map((guitarra) => (
+              <Guitarra key={guitarra.id} guitarra={guitarra.attributes} />
+            ))}
+          </div>
+        </main>
+        <section className="contenedor">
+          <h2 className="heading">Últimos Posts</h2>
+          <div className={styles.grid}>
+            {posts?.map((post) => (
+              <Post key={post.id} post={post.attributes} />
+            ))}
+          </div>
+        </section>
       </Layout>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const urlGuitarras = `${process.env.API_URL}/guitarras?populate=imagen`;
+  const urlPosts = `${process.env.API_URL}/posts?populate=imagen`;
+  const [resGuitarras, resPosts] = await Promise.all([
+    fetch(urlGuitarras),
+    fetch(urlPosts),
+  ]);
+
+  const [{ data: guitarras }, { data: posts }] = await Promise.all([
+    resGuitarras.json(),
+    resPosts.json(),
+  ]);
+
+  return {
+    props: {
+      guitarras,
+      posts,
+    },
+  };
 }
