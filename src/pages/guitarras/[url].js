693957dcb1b6b5570d4d6bdf61487be486fd4935
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-05-09 09:22:08 -0300

    Agregando un botón para añadir al carrito

--- src/pages/guitarras/[url].js
+++ src/pages/guitarras/[url].js
@@ -3,6 +3,7 @@ import React from "react";
 import Layout from "@/components/layout";
 import styles from "@/styles/Guitarras.module.css";
 
+const CANTIDAD_GUITARRAS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const Producto = ({ guitarra }) => {
   const { nombre, precio, descripcion, imagen } = guitarra[0].attributes;
   return (
@@ -18,6 +19,19 @@ const Producto = ({ guitarra }) => {
           <h3>{nombre}</h3>
           <p className={styles.descripcion}>{descripcion}</p>
           <p className={styles.precio}>${precio}</p>
+
+          <form htmlFor="cantidad" className={styles.formulario}>
+            <label>Cantidad</label>
+            <select id="cantidad">
+              <option value="0">-- Seleccione --</option>
+              {CANTIDAD_GUITARRAS?.map((cantidad) => (
+                <option value={cantidad} key={cantidad}>
+                  {cantidad}
+                </option>
+              ))}
+            </select>
+            <input type="submit" value="Agregar al carrito" />
+          </form>
         </div>
       </div>
     </Layout>
